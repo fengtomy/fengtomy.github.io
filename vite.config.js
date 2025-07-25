893b3e5a13450713c388c706ca95aa8d2251192b
xiaohu.feng <xiaohu.feng@centific.com>
2025-07-25 10:56:51 +0800

    feat: move react-markdown to vendor chunk

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router']
+          vendor: ['react', 'react-dom', 'react-router', 'react-markdown']
         }
       }
     }
