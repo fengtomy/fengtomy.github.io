c594265877f1b8667b229f05636a4a86a1b0c80a
xiaohu.feng <xiaohu.feng@centific.com>
2025-07-25 16:40:57 +0800

    feat: sort blog posts

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,16 +3,21 @@ import { NavLink } from "react-router"
 function Home() {
   return (
     <>
-      <h1>Welcome to fengxh blog posts.</h1>
-      <nav style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
-        <NavLink to="/javascript-map">Map in JavaScript</NavLink>
-        <NavLink to="/understand-cors">Understand CORS</NavLink>
-        <NavLink to="/websocket-intro">Websocket setup introduction</NavLink>
-        <NavLink to="/vue-clean-code">Clean code in Vue</NavLink>
-        <NavLink to="/new-in-ecma2025">What's new in ECMA2025</NavLink>
-        <NavLink to="/new-in-ecma2024">What's new in ECMA2024</NavLink>
-        <NavLink to="/new-in-ecma2023">What's new in ECMA2023</NavLink>
-      </nav>
+      <h1>Welcome to my online space.</h1>
+      <header>
+        <h2>
+          Blog Posts
+        </h2>
+        <nav style={{ display: 'flex', flexDirection: 'column', gap: 8, alignItems: 'flex-start' }}>
+          <NavLink to="/javascript-map">Map in JavaScript</NavLink>
+          <NavLink to="/websocket-intro">Websocket setup introduction</NavLink>
+          <NavLink to="/vue-clean-code">Clean code in Vue</NavLink>
+          <NavLink to="/understand-cors">Understand CORS</NavLink>
+          <NavLink to="/new-in-ecma2025">What's new in ECMA2025</NavLink>
+          <NavLink to="/new-in-ecma2024">What's new in ECMA2024</NavLink>
+          <NavLink to="/new-in-ecma2023">What's new in ECMA2023</NavLink>
+        </nav>
+      </header>
     </>
   )
 }
