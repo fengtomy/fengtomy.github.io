666bba106622fe0fe201a25b5b2323f338c77985
xiaohu.feng <xiaohu.feng@centific.com>
2025-08-05 17:53:53 +0800

    chore: re-order posts

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,12 +11,12 @@ function Home() {
         <nav style={{ display: 'flex', flexDirection: 'column', gap: 8, alignItems: 'flex-start' }}>
           <NavLink to="/attach-event-listener-with-ref">Attach event listener in React, with ref?</NavLink>
           <NavLink to="/javascript-map">Map in JavaScript</NavLink>
-          <NavLink to="/websocket-intro">Websocket setup introduction</NavLink>
-          <NavLink to="/vue-clean-code">Clean code in Vue</NavLink>
           <NavLink to="/understand-cors">Understand CORS</NavLink>
           <NavLink to="/new-in-ecma2025">What's new in ECMA2025</NavLink>
           <NavLink to="/new-in-ecma2024">What's new in ECMA2024</NavLink>
           <NavLink to="/new-in-ecma2023">What's new in ECMA2023</NavLink>
+          <NavLink to="/websocket-intro">Websocket setup introduction</NavLink>
+          <NavLink to="/vue-clean-code">Clean code in Vue</NavLink>
         </nav>
       </header>
     </>
