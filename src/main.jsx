5b688f095ea3c0b38773582d1111f2d6c68577c4
xiaohu.feng <xiaohu.feng@centific.com>
2025-08-09 17:20:33 +0800

    fix: router structure

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import Home from './Home.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import PostsHome from './blog-posts'
+import Layout from './Layout.jsx'
 
 const AddEventListenerWithRef = lazy(() => import('./blog-posts/views/AddEventListenerWithRef.jsx'))
 const JavaScriptMap = lazy(() => import('./blog-posts/views/JavaScriptMap.jsx'))
@@ -15,21 +16,27 @@ const NewInECMA2024 = lazy(() => import('./blog-posts/views/NewInECMA2024.jsx'))
 const NewInECMA2023 = lazy(() => import('./blog-posts/views/NewInECMA2023.jsx'))
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { 
-    path: 'blog-post',
-    element: <PostsHome />,
+  {
+    path: '/',
+    element: <Layout />,
     children: [
-      { path: 'attach-event-listener-with-ref', element: <AddEventListenerWithRef /> },
-      { path: 'javascript-map', element: <JavaScriptMap /> },
-      { path: 'understand-cors', element: <UnderstandCORS /> },
-      { path: 'websocket-intro', element: <WebsocketIntro /> },
-      { path: 'vue-clean-code', element: <VueCleanCode /> },
-      { path: 'new-in-ecma2025', element: <NewInECMA2025 /> },
-      { path: 'new-in-ecma2024', element: <NewInECMA2024 /> },
-      { path: 'new-in-ecma2023', element: <NewInECMA2023 /> },
+      { index: true, Component: Home },
+      {
+        path: 'blog-post',
+        Component: PostsHome,
+        children: [
+          { path: 'attach-event-listener-with-ref', element: <AddEventListenerWithRef /> },
+          { path: 'javascript-map', element: <JavaScriptMap /> },
+          { path: 'understand-cors', element: <UnderstandCORS /> },
+          { path: 'websocket-intro', element: <WebsocketIntro /> },
+          { path: 'vue-clean-code', element: <VueCleanCode /> },
+          { path: 'new-in-ecma2025', element: <NewInECMA2025 /> },
+          { path: 'new-in-ecma2024', element: <NewInECMA2024 /> },
+          { path: 'new-in-ecma2023', element: <NewInECMA2023 /> },
+        ]
+      }
     ]
-  },
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
