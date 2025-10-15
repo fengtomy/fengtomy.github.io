19ad0da11944a5bb3bf11b8a6ecf8242def78837
fengxh <xiaohu.feng@centific.com>
2025-10-15 14:22:09 +0800

    chore: add version upgrade scripts

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,25 +1,47 @@
 import fs from 'node:fs'
 
-const [vStr] = process.argv.slice(2)
-const v = vStr.split('=')[1]
+main()
 
-fs.readFile('package.json', 'utf-8', function(err, res) {
-    const obj = JSON.parse(res)
-    let [major, minor, patch] = obj.version.split('.').map(Number)
-    if (v === 'major') {
+function main() {
+    const [vStr] = process.argv.slice(2)
+
+    if (!vStr) {
+        throw new Error('Please pass into "version=major(or minor, patch)"')
+    }
+
+    const v = vStr.split('=')[1]
+
+    fs.readFile('package.json', 'utf-8', function(err, res) {
+        if (err) {
+            throw new Error('Failed to read package json content', err)
+        }
+
+        const obj = JSON.parse(res)
+
+        const newVersion = upgradeVersion(obj.version, v)
+
+        obj.version = newVersion
+
+        // Temporarily rewrite the whole file.
+        fs.writeFile('package.json', JSON.stringify(obj, null, 2), (err) => {
+            if (err) {
+                throw new Error('Failed to update package version.', err)
+            }
+            console.log('Successfully upgrade the package version')
+        })
+    })
+}
+
+function upgradeVersion(version, vPos) {
+    let [major, minor, patch] = version.split('.').map(Number)
+
+    if (vPos === 'major') {
         major += 1
-    } else if (v === 'minor') {
+    } else if (vPos === 'minor') {
         minor += 1
-    } else if (v === 'patch') {
+    } else if (vPos === 'patch') {
         patch += 1
     }
 
-    obj.version = [major, minor, patch].join('.')
-
-    // Contemporarily rewrite the whole file.
-    fs.writeFile('package.json', JSON.stringify(obj, null, 2), (err) => {
-        if (err) {
-            console.error('Failed to update package version.', err)
-        }
-    })
-})
+    return [major, minor, patch].join('.')
+}
